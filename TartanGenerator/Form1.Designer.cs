ad29263ea44def41e216dee3e039be725503dee5
BenZour <ben.zour1@gmail.com>
2024-03-30 21:16:38 +0000

    Add project files.

---
+++ TartanGenerator/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace TartanGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Threadcount:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 6);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(999, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Location = new System.Drawing.Point(280, 132);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 75);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Colour palette:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 43);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(999, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(141, 75);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(119, 20);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Reflective setts";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(280, 75);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(122, 20);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "Repeating setts";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(968, 73);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 28);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Export to PNG";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 740);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Tartan generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TartanGenerator/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TartanGenerator
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, SolidBrush> solidColours;
+        Dictionary<string, HatchBrush> patternColours;
+
+        public Form1()
+        {
+            InitializeComponent();
+            solidColours = new Dictionary<string, SolidBrush>();
+            patternColours = new Dictionary<string, HatchBrush>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                parseColourPalette();
+                (float widthFactor, float heightFactor) = computeSizeFactor();
+
+                string threadcount = "";
+                string pattern = @"(?<colour_code>\p{Lu}+)(?<threads>\d+)";
+                Regex regex = new Regex(pattern);
+
+
+                // Reflecting setts
+                if (radioButton1.Checked)
+                {
+                    Match match = regex.Match(textBox1.Text);
+
+                    Stack<string> threadStack = new Stack<string>();
+                    while (match.Success)
+                    {
+                        string threadString = match.Groups["colour_code"].Value + match.Groups["threads"].Value;
+                        threadcount += threadString;
+                        threadStack.Push(threadString);
+                        match = match.NextMatch();
+                    }
+                    while (threadStack.Count > 0)
+                    {
+                        threadcount += threadStack.Pop();
+                    }
+                }
+                // Repeating setts
+                else
+                {
+                    threadcount = String.Concat(textBox1.Text, textBox1.Text);
+                }
+
+
+                Match matchedThread = regex.Match(threadcount);
+
+                float carriageLocation = 0;
+                while (matchedThread.Success)
+                {
+
+                    float threads = Int32.Parse(matchedThread.Groups["threads"].Value);
+                    threads *= widthFactor;
+                    string colour_code = matchedThread.Groups["colour_code"].Value;
+                    e.Graphics.FillRectangle(
+                        solidColours[colour_code],
+                        carriageLocation,
+                        0,
+                        threads,
+                        panel1.Height
+                        );
+                    carriageLocation += threads;
+                    matchedThread = matchedThread.NextMatch();
+                }
+
+                matchedThread = regex.Matches(threadcount)[0];
+
+                carriageLocation = 0;
+                while (matchedThread.Success)
+                {
+                    float threads = Int32.Parse(matchedThread.Groups["threads"].Value);
+                    threads *= heightFactor;
+                    string colour_code = matchedThread.Groups["colour_code"].Value;
+                    e.Graphics.FillRectangle(
+                        patternColours[colour_code],
+                        0,
+                        carriageLocation,
+                        panel1.Width,
+                        threads
+                        );
+                    carriageLocation += threads;
+                    matchedThread = matchedThread.NextMatch();
+                }
+            }
+        }
+
+        private (float,float) computeSizeFactor()
+        {
+            string pattern = @"\d+";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(textBox1.Text);
+
+            int sum = 0;
+            foreach (Match m in matches)
+            {
+                sum += Int32.Parse(m.Value);
+            }
+
+            return (panel1.Width / (sum * 2f), panel1.Height / (sum * 2f));
+        }
+
+        private void parseColourPalette()
+        {
+            solidColours.Clear();
+            patternColours.Clear();
+            string pattern = @"(?<colour>\w+)=(?<rgb_code>\S{6})\w+";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(textBox2.Text);
+
+            while (match.Success)
+            {
+                SolidBrush solidBrush = new SolidBrush(
+                    ColorTranslator.FromHtml("#" + match.Groups["rgb_code"].Value)
+                    );
+                HatchBrush hatchBrush = new HatchBrush(
+                    HatchStyle.DarkDownwardDiagonal,
+                    Color.Transparent,
+                    ColorTranslator.FromHtml("#" + match.Groups["rgb_code"].Value)
+                    );
+
+                solidColours.Add(match.Groups["colour"].Value, solidBrush);
+                patternColours.Add(match.Groups["colour"].Value, hatchBrush);
+
+                match = match.NextMatch();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (Bitmap bmp = new Bitmap(panel1.Width, panel1.Height))
+            {
+                panel1.DrawToBitmap(bmp, new Rectangle(0, 0, panel1.Width, panel1.Height));
+                bmp.Save("tartan_export.png", ImageFormat.Png);
+            }
+        }
+    }
+}
